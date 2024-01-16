bb217ccff5da2eb166acf33bc52383f8d0d3594c
matheusman <pereiralimamatheuspl@gmail.com>
2024-01-16 15:05:35 -0300

    adicionando o dotenv

---
+++ src/graphql/user/datalouder.ts
@@ -0,0 +1,5 @@
+
+
+class UserDataLouder extends DataLoader {
+
+}
--- src/graphql/user/userResolvers.ts
+++ src/graphql/user/userResolvers.ts
@@ -5,41 +5,42 @@ import { UserError } from './interface/userError.interface';
 import { userInterface } from './interface/user.interface';
 import { checkInterface } from '../../checkInterfaceGeneric';
 
+
 const user = async (
-  _: unknown,
-  { id }: { id: string },
-  { getUser }: contextInterface
+	_: unknown,
+	{ id }: { id: string },
+	{ getUser }: contextInterface
 ): Promise<userInterface | UserError> => {
-  const request = await getUser('/' + id);
-  const user = request.data;
-  if (checkInterface<userInterface>(user, 'id')) {
-    return {
-      statusCode: 404,
-      message: 'Usuario não existe',
-    };
-  }
-  return user;
+	const request = await getUser('/' + id);
+	const user = request.data;
+	if (checkInterface<userInterface>(user, 'id')) {
+		return {
+			statusCode: 404,
+			message: 'Usuario não existe',
+		};
+	}
+	return user;
 };
 
 const users = async (
-  _: unknown,
-  { input }: { input: apiFilterInterface },
-  { getUsers }: contextInterface
+	_: unknown,
+	{ input }: { input: apiFilterInterface },
+	{ getUsers }: contextInterface
 ): Promise<userInterface[]> => {
-  const params = new URLSearchParams(input as URLSearchParams | undefined);
-  const users = await getUsers('/?' + params.toString());
-  const data = await users.data;
-  return data;
+	const params = new URLSearchParams(input as URLSearchParams | undefined);
+	const users = await getUsers('/?' + params.toString());
+	const data = await users.data;
+	return data;
 };
 
 export const userResolvers = {
-  Query: {
-    user,
-    users,
-  },
-  UserResult: {
-    __resolveType: (obj: unknown) => {
-      return checkInterface<UserError>(obj, 'statusCode')
-    },
-  },
+	Query: {
+		user,
+		users,
+	},
+	UserResult: {
+		__resolveType: (obj: unknown) => {
+			return checkInterface<UserError>(obj, 'statusCode');
+		},
+	},
 };
